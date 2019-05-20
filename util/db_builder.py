ce4ec6107108200f7f1fc81933cbaccb201c9c55
Tim Marder <tmarder@stuy.edu>
2019-05-20 12:32:39 -0400

    need to fix db python scripts

--- util/db_builder.py
+++ util/db_builder.py
@@ -1,6 +1,62 @@
+#BATTery
+#Thomas Lee (PM)
+#Britni Canale
+#Ahnaf Kazi
+#Tim Marder
+#p06
+
 '''
 This script creates a database with the necessary tables for data to be stored
 '''
+
+import sqlite3 #imports sqlite
+
+DB_FILE="data/BATT.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor() #facilitates db operations
+
+
+def users(): #creates the users db
+    command = "CREATE TABLE users(username TEXT, password TEXT)"
+    c.execute(command)
+
+def articles(): #create the articles db
+    command = "CREATE TABLE articles(article_id INTEGER, title TEXT, text TEXT, numlikes INTEGER, users TEXT)"
+    c.execute(command)
+
+def comments(): #creates the comments db
+    command = "CREATE TABLE comments(comment_id INTEGER, article_id INTEGER, commenter TEXT, text TEXT)"
+    c.execute(command)
+
+def daily(): #creates the daily db
+    command = "CREATE TABLE daily(date TEXT, cat TEXT, dog TEXT, recipe TEXT, word TEXT, defi TEXT, weather TEXT, temperature TEXT)"
+    c.execute(command)
+
+def pref(): #creates the pref db
+#    command = "CREATE TABLE pref(user TEXT, source TEXT, daily TEXT)"
+    command = "CREATE TABLE pref(user TEXT, preffered TEXT, type TEXT)"
+    c.execute(command)
+
+
+def main(): #calls all of the functions to build the databases
+    try:
+        users()
+        articles()
+        comments()
+        daily()
+        pref()
+        print("done")
+    except:
+        pass
+
+
+main()
+
+
+'''
+This script creates a database with the necessary tables for data to be stored
+
 
 import sqlite3 #imports sqlite
 
@@ -19,3 +75,4 @@ def main(): #calls all of the functions to build the databases
 
 
 main()
+'''
